7d7f9ae222c1ed14e8664fc5da8eca4bad357205
Brahim Laarif <brahim.laarif@gmail.com>
2018-10-13 05:37:31 +0100

    Generate dump sql file

---
+++ Safebet.WebAPI/dump.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE `__EFMigrationsHistory` (
+    `MigrationId` varchar(95) NOT NULL,
+    `ProductVersion` varchar(32) NOT NULL,
+    CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
+);
+
+CREATE TABLE `Matches` (
+    `Id` int NOT NULL AUTO_INCREMENT,
+    `Hash` longtext NULL,
+    `Event` longtext NOT NULL,
+    `Name` longtext NOT NULL,
+    `StartDate` datetime(6) NOT NULL,
+    `LastTimePointHash` longtext NULL,
+    `CreatedDate` datetime(6) NOT NULL,
+    `LastModifiedDate` datetime(6) NOT NULL,
+    CONSTRAINT `PK_Matches` PRIMARY KEY (`Id`)
+);
+
+CREATE TABLE `TimePoints` (
+    `Id` int NOT NULL AUTO_INCREMENT,
+    `Hash` longtext NULL,
+    `HomeOdds` double NOT NULL,
+    `DrawOdds` double NOT NULL,
+    `AwayOdds` double NOT NULL,
+    `CreatedDate` datetime(6) NOT NULL,
+    `MatchId` int NULL,
+    CONSTRAINT `PK_TimePoints` PRIMARY KEY (`Id`),
+    CONSTRAINT `FK_TimePoints_Matches_MatchId` FOREIGN KEY (`MatchId`) REFERENCES `Matches` (`Id`) ON DELETE NO ACTION
+);
+
+CREATE INDEX `IX_TimePoints_MatchId` ON `TimePoints` (`MatchId`);
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20181013041937_InitialModel', '2.1.1-rtm-30846');
+
